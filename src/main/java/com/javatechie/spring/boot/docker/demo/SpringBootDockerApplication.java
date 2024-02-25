bdbe8bd7550544abda6cb24759300b9030ae909c
pugazh ram <pugazhramram@gmail.com>
2024-02-25 17:01:22 +0530

    test jenkins

--- src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
+++ src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//test jenkins commitg
 @SpringBootApplication
 @RestController
 public class SpringBootDockerApplication {
